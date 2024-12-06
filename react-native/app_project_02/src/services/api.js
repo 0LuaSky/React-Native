f0e77f155f1168f84d5be30ea687af62545ee323
Luana H.R. <124940006+0LuaSky@users.noreply.github.com>
2024-12-06 15:29:53 -0300

    Update api.js

--- react-native/app_project_02/src/services/api.js
+++ react-native/app_project_02/src/services/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://viacep.com.br/ws/'
+  baseURL: 'https://pokeapi.co/api/v2/',
+  timeout: 10000, // Define um limite de tempo para as requisições
 });
 
+// Interceptador para tratar erros
+api.interceptors.response.use(
+  response => response,
+  error => {
+    console.error('Erro na requisição:', error.response || error.message);
+    return Promise.reject(error);
+  }
+);
 
 export default api;
